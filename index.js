f141b02191d4ae8d49c19ed5b1bd8feec1c38888
Carl-Robert <robertm6ts@gmail.com>
2024-11-29 12:28:25 +0200

    Create form requrest response with API data

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const exp = require('constants')
 const express = require('express')
 const path = require('path')
 
@@ -7,6 +8,9 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/boostrap/dist')))
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 const key = '8be14fc27071cc1cd95870ea6a27e429'
 let city = 'Tartu'
@@ -26,7 +30,25 @@ app.get('/', (req, res) =>{
             temp: temp
         })
     })
-   
+
 } )
+app.post('/', (req,res) => {
+    let city = req.body.cityname
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+    .then((response)=> {
+        return response.json()
+    })
+    .then ((data) => {
+        let description = data.weather[0].description
+        let city = data.name
+        let temp = Math.round(parseFloat(data.main.temp)-274.29)
+        res.render('index', {
+            description: description,
+            city: city,
+            temp: temp
+        })
+    })
+    
+})
 
 app.listen(3002)
