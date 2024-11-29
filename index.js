1509eaace84383ce5bacdf62f0fee5eecae39c9c
Carl-Robert <robertm6ts@gmail.com>
2024-11-29 13:11:47 +0200

    Refactor code duplicate with promise for data fetching

--- index.js
+++ index.js
@@ -13,25 +13,58 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 const key = '8be14fc27071cc1cd95870ea6a27e429'
-let city = 'Tartu'
 
-app.get('/', (req, res) =>{
-    fetch (`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
-    .then((response) =>{
-        return response.json()
-    })
-    .then((data) => {
-        let description = data.weather[0].description
-        let city = data.name
-        let temp = Math.round(parseFloat(data.main.temp)- 274.29) 
-        res.render('index', {
-            description: description,
-            city: city,
-            temp: temp
+
+const getWeatherData = (city) => {
+    return new Promise((resolve,reject) =>{
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
+        fetch(url)
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            let description = data.weather[0].description
+            let city = data.name
+            let temp = Math.round(parseFloat(data.main.temp)- 274.29) 
+            const result = {
+                description: description,
+                city: city,
+                temp: temp
+            }
+            resolve(result)
+        })
+        .catch(error => {
+            reject(error)
         })
     })
+}
+app.all('/', (req, res) =>{
+    let city
+    if(req.method == 'GET'){
+        city = 'Tartu'
+    } else if (req.method == 'POST'){
+        city = req.body.cityname
+    }
+    getWeatherData(city)
+    .then((data)=>{
+        res.render('index', data)
+    })
+})
+app.get('/', (req,res) =>{
+    let city = 'Tartu'
+    getWeatherData(city)
+    .then((data)=> {
+        res.render('index', data)
+    })
+})
+app.post('/', (req, res) => {
+    let city = req.body.cityname
+    getWeatherData(city)
+    .then((data) =>{
+        res.render('index', data)
+    })
+})
 
-} )
 app.post('/', (req,res) => {
     let city = req.body.cityname
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
