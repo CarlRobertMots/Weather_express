de6a0598c950acee9f0d3c94d894101b5095f597
Carl-Robert <robertm6ts@gmail.com>
2024-11-29 13:42:22 +0200

    Create API error handling

--- index.js
+++ index.js
@@ -1,87 +1,74 @@
-const exp = require('constants')
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
 
-const app = express()
+const app = express();
 
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
+const key = '8be14fc27071cc1cd95870ea6a27e429';
 
-app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/boostrap/dist')))
+// Set up view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+// Middleware
+app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const key = '8be14fc27071cc1cd95870ea6a27e429'
-
-
+// Fetch weather data
 const getWeatherData = (city) => {
-    return new Promise((resolve,reject) =>{
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
+    return new Promise((resolve, reject) => {
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
         fetch(url)
-        .then((response) => {
-            return response.json()
-        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('City not found');
+                }
+                return response.json();
+            })
+            .then((data) => {
+                if (!data.weather || !data.main) {
+                    throw new Error('Invalid data from API');
+                }
+                let description = data.weather[0].description;
+                let city = data.name;
+                let temp = Math.round(parseFloat(data.main.temp) - 273.15);
+                resolve({
+                    description,
+                    city,
+                    temp,
+                    error: null,
+                });
+            })
+            .catch((error) => {
+                resolve({ error: error.message });
+            });
+    });
+};
+
+// Routes
+app.get('/', (req, res) => {
+    let city = 'Tartu';
+    getWeatherData(city)
         .then((data) => {
-            let description = data.weather[0].description
-            let city = data.name
-            let temp = Math.round(parseFloat(data.main.temp)- 274.29) 
-            const result = {
-                description: description,
-                city: city,
-                temp: temp
-            }
-            resolve(result)
+            res.render('index', data);
         })
-        .catch(error => {
-            reject(error)
-        })
-    })
-}
-app.all('/', (req, res) =>{
-    let city
-    if(req.method == 'GET'){
-        city = 'Tartu'
-    } else if (req.method == 'POST'){
-        city = req.body.cityname
-    }
-    getWeatherData(city)
-    .then((data)=>{
-        res.render('index', data)
-    })
-})
-app.get('/', (req,res) =>{
-    let city = 'Tartu'
-    getWeatherData(city)
-    .then((data)=> {
-        res.render('index', data)
-    })
-})
+        .catch(() => {
+            res.render('index', { error: 'Problem with getting data, try again...' });
+        });
+});
+
 app.post('/', (req, res) => {
-    let city = req.body.cityname
+    let city = req.body.cityname;
     getWeatherData(city)
-    .then((data) =>{
-        res.render('index', data)
-    })
-})
-
-app.post('/', (req,res) => {
-    let city = req.body.cityname
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
-    .then((response)=> {
-        return response.json()
-    })
-    .then ((data) => {
-        let description = data.weather[0].description
-        let city = data.name
-        let temp = Math.round(parseFloat(data.main.temp)-274.29)
-        res.render('index', {
-            description: description,
-            city: city,
-            temp: temp
+        .then((data) => {
+            res.render('index', data);
         })
-    })
-    
-})
+        .catch(() => {
+            res.render('index', { error: 'Problem with getting data, try again...' });
+        });
+});
 
-app.listen(3002)
+// Start server
+app.listen(3002, () => {
+    console.log('Server running on http://localhost:3002');
+});
